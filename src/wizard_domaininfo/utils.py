51d24bfe511d47b7cf9c04c29a47843186131470
Michael Ramsey <mike@hackerdise.me>
2021-03-06 18:51:49 -0500

    Updated tests and settings

--- src/wizard_domaininfo/utils.py
+++ src/wizard_domaininfo/utils.py
@@ -3,9 +3,17 @@ from requests.exceptions import Timeout, ConnectionError
 from requests.packages.urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 
+
 # https://findwork.dev/blog/advanced-usage-python-requests-timeouts-retries-hooks/
 class TimeoutHTTPAdapter(HTTPAdapter):
     def __init__(self, *args, **kwargs):
+        """The HTTPAdapter is comparable we can combine retries and timeouts.
+
+        :param *args: args
+        :type *args: mixed
+        :param **kwargs: Some kwargs
+        :type **kwargs: mixed
+        """
         DEFAULT_TIMEOUT = 1
         self.timeout = DEFAULT_TIMEOUT
         if "timeout" in kwargs:
@@ -14,6 +22,15 @@ class TimeoutHTTPAdapter(HTTPAdapter):
         super().__init__(*args, **kwargs)
 
     def send(self, request, **kwargs):
+        """Send a request via TimeoutHTTPAdapter.
+
+        Args:
+            request (): request
+            **kwargs (): kwargs
+
+        Returns: Boolean
+
+        """
         timeout = kwargs.get("timeout")
         if timeout is None:
             kwargs["timeout"] = self.timeout
@@ -28,13 +45,23 @@ http.mount("https://", TimeoutHTTPAdapter(max_retries=retries))
 http.mount("http://", TimeoutHTTPAdapter(max_retries=retries))
 
 TIMEOUT = 1.0  # timeout in seconds
+
+
 # wizard_whois.net.socket.setdefaulttimeout(TIMEOUT)
 
 
 def check_http_reachable(domain_name):
-    """Is the domain reachable via http?"""
+    """Check if the domain is reachable via http.
+
+    Args:
+        domain_name (): domain_name Domain name to check is reachable.
+
+    Returns:
+        Boolean
+
+    """
     try:
-        if not "http" in domain_name:
+        if "http" not in domain_name:
             url = "http://" + domain_name
         http.get(url)
         return True
@@ -44,14 +71,22 @@ def check_http_reachable(domain_name):
 
 
 def check_url_ssl(domain_name):
-    """Is the domain reachable via http?"""
+    """Check if the domain has a valid SSL certificate.
+
+    Args:
+        domain_name (): domain_name Domain name to check SSL for.
+
+    Returns:
+        Boolean
+
+    """
     try:
-        if not "http" in domain_name:
+        if "http" not in domain_name:
             url = "https://" + domain_name
         else:
             url = domain_name
-        http.get(url)
+        http.get(url, verify=True)
         return True
     except requests.exceptions.ConnectionError:
-        print(f"URL {url} not reachable")
+        print(f"URL {url} not reachable or SSL expired")
         return False
