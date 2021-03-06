fa8efadd25c2233434a66e70e38b9c8141e97458
Michael Ramsey <mike@hackerdise.me>
2021-03-06 11:22:38 -0500

    Restructured with copier-poetry

---
+++ src/wizard_domaininfo/__init__.py
@@ -0,0 +1,11 @@
+"""
+Wizard Domaininfo package.
+
+DNS/Whois Domain Information library
+"""
+
+from typing import List
+
+__all__: List[str] = []  # noqa: WPS410 (the only __variable__ we use)
+
+__version__ = "0.1.0"
---
+++ src/wizard_domaininfo/utils.py
@@ -0,0 +1,18 @@
+import tomlkit
+import os,sys
+from pathlib import Path
+# path = Path(__file__).parent.absolute()
+# path_2dir = os.path.abspath('../../')
+# pyproject_path = os.path.join(os.path.abspath(path), path_2dir, 'pyproject.toml')
+#
+# def _get_project_meta(pyproject_path):
+#     with open(pyproject_path) as pyproject:
+#         file_contents = pyproject.read()
+#
+#     return tomlkit.parse(file_contents)['tool']['poetry']
+#
+#
+# print(path)
+#
+#
+# print(pyproject_path)
